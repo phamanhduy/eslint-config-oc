631f9b3288c137efbac42caaffc2c18416807a76
phamanhduy <duypham965@gmail.com>
2020-10-04 16:44:24 +0700

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "newline-per-chained-call": 0,
+    "no-underscore-dangle": 0,
+    "consistent-return": 0,
+    "no-await-in-loop": 0,
+    "no-restricted-syntax": 0,
+    "no-param-reassign": 0,
+    "prefer-destructuring": 0,
+    "no-console": 2,
+    "arrow-parens": 0,
+    "prefer-promise-reject-errors": 0,
+    "no-use-before-define": 0
+  },
+  "env": {
+    "node": true,
+    "mocha": true
+  }
+}
